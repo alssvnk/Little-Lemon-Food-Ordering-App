3c1d8e59d05d8c59150ec66315b24d53f789d965
alssvnk <49952551+alssvnk@users.noreply.github.com>
2024-10-04 10:58:11 +0200

    Add files via upload

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Alisiya on 19.09.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
